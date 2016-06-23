5102f4cbae64f13664e2bc943531cda5422ccd9a
Nathan Perkins <nathan@maxmo.net>
2016-06-23 12:20:30 -0400

    Initial Commit

---
+++ VectorMath/VectorMath.h
@@ -0,0 +1,19 @@
+//
+//  VectorMath.h
+//  VectorMath
+//
+//  Created by Nathan Perkins on 6/23/16.
+//  Copyright © 2016 MaxMo Technologies LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VectorMath.
+FOUNDATION_EXPORT double VectorMathVersionNumber;
+
+//! Project version string for VectorMath.
+FOUNDATION_EXPORT const unsigned char VectorMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VectorMath/PublicHeader.h>
+
+
